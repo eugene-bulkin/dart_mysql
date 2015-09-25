5b2af4a4d509ee3e41f84d64f25d3e22c21b771c
Eugene Bulkin <ebulkin@caltech.edu>
2015-09-25 17:52:23 -0500

    First iteration of Coveralls support...

---
+++ tool/travis.sh
@@ -0,0 +1,10 @@
+pub run test
+
+if [ "$REPO_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $REPO_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/**/*
+fi
