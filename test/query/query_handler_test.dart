7a44d4303503e284e6dc751240bf488a435a7cdd
Eugene Bulkin <ebulkin@caltech.edu>
2015-09-26 19:58:55 -0500

    Set up further query tests and add to all_tests.dart file.

---
+++ test/query/query_handler_test.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:dart_mysql/protocol/column_type.dart';
+import 'package:dart_mysql/query/query_handler.dart';
+import 'package:dart_mysql/query/result_row.dart';
+import 'package:test/test.dart';
+
+main() {
+  group('QueryHandler', () {
+  });
+}
---
+++ test/query/result_row_test.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+import 'package:dart_mysql/protocol/column_type.dart';
+import 'package:dart_mysql/query/result_row.dart';
+import 'package:test/test.dart';
+
+main() {
+  group('ResultRow', () {
+  });
+}
